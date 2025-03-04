f062f4251edffbeeb39fc1efafdee57507668515
BaconBites1673 <143040944+BaconBites1673@users.noreply.github.com>
2025-03-04 11:24:51 -0600

    Update account.h
    Added new functions:
    failedAtt: Keep track of tries attempted on that specific account so that you can't just simply reset the tries by moving to another account.
    check functions: Checks the amount of tries the account has left and that the usernames match
    display info: provide a way to display most info to the user

--- account.h
+++ account.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 class account{ //define account class to manipulate and store employee and costumer accounts
@@ -9,13 +10,18 @@ public:
 	void setPassword(string pass);
 	void setName(string n);
 	void setPhone(string digits);
+	void setPrivilege(string priv);
+	void failedAtt();
+	bool checkUser(string user);
 	bool checkTries();
 	//define display functions to display the information of the accounts to users.
 	void displayUsername();
 	void displayName();
 	void displayPhone();
+    void displayInfo();
+    void displayWelcome();
 	//Variables that will store account information.
-	string username, password, name, phone;
+	string username, password, name, phone, privilege;
 	int tries;
 };
 
@@ -47,9 +53,38 @@ void account::displayPhone() {
 	cout << "Phone number: " << phone;
 }
 
-// bool account::checkTries() {
-// 	if(tries < 3)
-// 		return TRUE;
-// 	else
-// 		return FALSE;
-// }
+bool account::checkTries() {
+	if(tries < 3)
+		return true;
+	else
+		return false;
+}
+
+void account::failedAtt(){
+    tries ++;
+}
+
+bool account::checkUser(string user){
+    if (user == username)
+        return true;
+    else
+        return false;
+}
+
+void account::setPrivilege(string priv){
+    privilege = priv;
+}
+
+void account::displayInfo(){
+    cout << "\n\n\tAccount information";
+    cout << endl << setw(30) << setfill('-') << "-" << setfill(' ');
+    cout << left;
+    cout << setw(18) << "\nName: " << name;
+    cout << setw(18) << "\nUsername: " << username;
+    cout << setw(18) << "\nPhone: " << phone;
+    cout << setw(18) << "\nAccount type: " << privilege;
+}
+
+void account::displayWelcome(){
+    cout << "\n\nWelcome " << name;
+}
