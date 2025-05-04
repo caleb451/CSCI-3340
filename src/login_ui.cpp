0443b5133c19928196c67094caa2a3396214c907
Caleb451 <gomezcaleb88@gmail.com>
2025-05-04 13:59:38 -0500

    commented out for test

--- src/login_ui.cpp
+++ src/login_ui.cpp
@@ -8,11 +8,11 @@
 
 using namespace std;
 
-string trim(const string& str) {
-    size_t start = str.find_first_not_of(" \t\r\n");
-    size_t end = str.find_last_not_of(" \t\r\n");
-    return (start == string::npos || end == string::npos) ? "" : str.substr(start, end - start + 1);
-}
+// string trim(const string& str) {
+//     size_t start = str.find_first_not_of(" \t\r\n");
+//     size_t end = str.find_last_not_of(" \t\r\n");
+//     return (start == string::npos || end == string::npos) ? "" : str.substr(start, end - start + 1);
+// }
 
 string symbolToPrivilege(char symbol) {
     switch (symbol) {
